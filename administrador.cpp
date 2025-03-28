1e33210f7374d7bcbb88f711e95653a9a294642d
Miqqueel <100472506@alumnos.uc3m.es>
2025-03-28 14:01:53 +0100

    Add files via upload

---
+++ administrador.cpp
@@ -0,0 +1,16 @@
+#include "administrador.h"
+
+
+Administrador::Administrador(): Usuario(){}
+
+
+
+Administrador::Administrador(string nombreUsuario, string contraseña): Usuario(nombreUsuario, contraseña){}
+
+
+
+Administrador::Administrador(const Administrador& A): Usuario(A){}
+
+
+
+Administrador::~Administrador(){}
